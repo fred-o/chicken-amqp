06033041e7de4db5daffbd0cba7fc31490799bb2
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2019-10-24 21:18:32 +0200

    finally made amqp091 its own module

---
+++ amqp091.scm
@@ -0,0 +1,118 @@
+(module amqp091 *
+  (import scheme
+          (chicken base)
+          (chicken syntax)
+          (chicken format)
+          (chicken bitwise)
+          srfi-1
+          bitstring)
+
+  (define-record frame type channel class-id method-id properties payload)
+
+  (define-record-printer (frame frm out)
+    (fprintf out "#<frame type:~S channel:~S class:~S method:~S>"
+             (frame-type frm)
+             (frame-channel frm)
+             (frame-class-id frm)
+             (frame-method-id frm)))
+
+  (define (encode-table props)
+    (apply bitstring-append
+           (map (lambda (prop)
+                  (let ((key (car prop))
+                        (val (cdr prop)))
+                    (cond ((string? val) (bitconstruct ((string-length key) 8) (key bitstring) (#\S) ((string-length val) 32) (val bitstring))))))
+                props)))
+
+  (define (parse-type bits)
+    (bitmatch
+     bits
+     (((#\F) (Size 32) (Table (* 8 Size) bitstring) (Rest bitstring)) (cons (parse-table Table) Rest))
+     (((#\S) (Size 32) (String (* 8 Size) bitstring) (Rest bitstring)) (cons (bitstring->string String) Rest))
+     (((#\t) (Bool boolean) (Rest bitstring)) (cons Bool Rest))
+     (((Type 8) (Rest bitstring)) (error "Unknown table type " Type))))
+
+  (define (parse-table bits)
+    (if (= 0 (bitstring-length bits))
+        '()
+        (bitmatch
+         bits
+         (((size 8)
+           (name (* size 8) bitstring)
+           (rest bitstring))
+          (let* ((result (parse-type rest))
+                 (value (car result))
+                 (rest (cdr result)))
+            (cons (cons (bitstring->string name) value) (parse-table rest)))))))
+
+(define (bit-value n pos) (if (bit->boolean n pos) 1 0))
+
+(define (parse-headers-payload bits)
+  (bitmatch
+   bits
+   (((class-id 16)
+     (weight 16)
+     (body-size 64)
+     (flags 16)
+     (content-type-size (* 8 (bit-value flags 15))) (content-type (* 8 content-type-size) bitstring)
+     (content-encoding-size (* 8 (bit-value flags 14))) (content-encoding (* 8 content-encoding-size) bitstring)
+     (headers-size (* 32 (bit-value flags 13))) (headers (* 8 headers-size) bitstring)
+     (delivery-mode (* 8 (bit-value flags 12)))
+     (priority (* 8 (bit-value flags 11)))
+     (correlation-id-size (* 8 (bit-value flags 10))) (correlation-id (* 8 correlation-id-size) bitstring)
+     (reply-to-size (* 8 (bit-value flags 9))) (reply-to (* 8 reply-to-size) bitstring)
+     (expiration-size (* 8 (bit-value flags 8))) (expiration (* 8 expiration-size) bitstring)
+     (message-id-size (* 8 (bit-value flags 7))) (message-id (* 8 message-id-size) bitstring)
+     (timestamp (* 64 (bit-value flags 6)))
+     (type-size (* 8 (bit-value flags 5))) (type (* 8 type-size) bitstring)
+     (user-id-size (* 8 (bit-value flags 4))) (user-id (* 8 user-id-size) bitstring)
+     (app-id-size (* 8 (bit-value flags 3))) (app-id (* 8 app-id-size) bitstring))
+    (list class-id 0
+          (list (cons 'content-type (bitstring->string content-type))
+                (cons 'content-encoding (bitstring->string content-encoding))
+                (cons 'headers (parse-table headers))
+                (cons 'delivery-mode delivery-mode)
+                (cons 'priority priority)
+                (cons 'correlation-id (bitstring->string correlation-id))
+                (cons 'reply-to (bitstring->string reply-to))
+                (cons 'expiration (bitstring->string expiration))
+                (cons 'message-id (bitstring->string message-id))
+                (cons 'timestamp timestamp)
+                (cons 'type (bitstring->string type))
+                (cons 'user-id (bitstring->string user-id))
+                (cons 'app-id (bitstring->string app-id)))))))
+
+(define (parse-frame str)
+  (bitmatch
+   str
+   ((("AMQP" 32 bitstring)
+     (Version bitstring))
+    (error "Server rejected protocol version"))
+   (((type 8)
+     (channel 16)
+     (payload-size 32)
+     (payload (* 8 payload-size) bitstring)
+     (#xce)
+     (rest bitstring))
+    (case type
+      ((1) (cons (apply make-frame (append (list type channel) (parse-method-payload payload) '(#f)))
+                 rest))
+      ((2) (cons (apply make-frame (append (list type channel) (parse-headers-payload payload) '(#f)))
+                 rest))
+      ((3) (cons (make-frame type channel 0 0 #f payload)
+                 rest))
+      ((8) (cons #f rest)) ;; This is a heartbeat
+      (else (error "Unimplemented type " type))))
+   (else
+    ;; no match (yet)
+    (cons #f str))))
+
+(define (encode-frame type channel payload)
+  (bitconstruct
+   (type 8)
+   (channel 16)
+   ((/ (bitstring-length payload) 8) 32)
+   (payload bitstring)
+   (#xce 8)))
+
+  (include "parser.scm"))
--- client.scm
+++ client.scm
@@ -1,3 +1,5 @@
+(include "amqp091.scm")
+
 (import scheme (chicken base) (chicken syntax)
         (chicken tcp)
         (chicken io)
@@ -8,9 +10,8 @@
         srfi-18
         srfi-88
         mailbox
-        bitstring)
-
-(include "parser.scm")
+        bitstring
+        amqp091)
 
 (define *amqp-header*
   (bitstring->string (bitconstruct
@@ -31,15 +32,6 @@
 (define-record-printer (channel ch out)
   (fprintf out "#<channel: ~S>" id))
 
-(define-record frame type channel class-id method-id properties payload)
-
-(define-record-printer (frame frm out)
-  (fprintf out "#<frame type:~S channel:~S class:~S method:~S>"
-           (frame-type frm)
-           (frame-channel frm)
-           (frame-class-id frm)
-           (frame-method-id frm)))
-
 ;; (define-record method-frame class-id method-id arguments)
 
 ;; (define-record header type channel class-id)
@@ -47,7 +39,6 @@
 ;; (define-record content payload)
 
 (define-record message type channel class-id class method-id method arguments)
-
 
 ;; Fns
 
@@ -58,105 +49,6 @@
     (connection-channel-id-seq-set! connection (+ 1 id))
     (mutex-unlock! lock)
     id))
-
-(define (encode-table props)
-   (apply bitstring-append
-          (map (lambda (prop)
-                 (let ((key (car prop))
-                       (val (cdr prop)))
-                   (cond ((string? val) (bitconstruct ((string-length key) 8) (key bitstring) (#\S) ((string-length val) 32) (val bitstring))))))
-               props)))
-
-(define (parse-type bits)
-  (bitmatch
-   bits
-   (((#\F) (Size 32) (Table (* 8 Size) bitstring) (Rest bitstring)) (cons (parse-table Table) Rest))
-   (((#\S) (Size 32) (String (* 8 Size) bitstring) (Rest bitstring)) (cons (bitstring->string String) Rest))
-   (((#\t) (Bool boolean) (Rest bitstring)) (cons Bool Rest))
-   (((Type 8) (Rest bitstring)) (error "Unknown table type " Type))))
-
-(define (parse-table bits)
-  (if (= 0 (bitstring-length bits))
-      '()
-      (bitmatch
-       bits
-       (((size 8)
-         (name (* size 8) bitstring)
-         (rest bitstring))
-        (let* ((result (parse-type rest))
-               (value (car result))
-               (rest (cdr result)))
-          (cons (cons (bitstring->string name) value) (parse-table rest)))))))
-
-(define (parse-frame str)
-  (bitmatch
-   str
-   ((("AMQP" 32 bitstring)
-     (Version bitstring))
-    (error "Server rejected protocol version"))
-   (((type 8)
-     (channel 16)
-     (payload-size 32)
-     (payload (* 8 payload-size) bitstring)
-     (#xce)
-     (rest bitstring))
-    (case type
-      ((1) (cons (apply make-frame (append (list type channel) (parse-method-payload payload) '(#f)))
-                 rest))
-      ((2) (cons (apply make-frame (append (list type channel) (parse-headers-payload payload) '(#f)))
-                 rest))
-      ((3) (cons (make-frame type channel 0 0 #f payload)
-                 rest))
-      ((8) (cons #f rest)) ;; This is a heartbeat
-      (else (error "Unimplemented type " type))))
-   (else
-    ;; no match (yet)
-    (cons #f str))))
-
-(define (encode-frame type channel payload)
-  (bitconstruct
-   (type 8)
-   (channel 16)
-   ((/ (bitstring-length payload) 8) 32)
-   (payload bitstring)
-   (#xce 8)))
-
-(define (bit-value n pos) (if (bit->boolean n pos) 1 0))
-
-(define (parse-headers-payload bits)
-  (bitmatch
-   bits
-   (((class-id 16)
-     (weight 16)
-     (body-size 64)
-     (flags 16)
-     (content-type-size (* 8 (bit-value flags 15))) (content-type (* 8 content-type-size) bitstring)
-     (content-encoding-size (* 8 (bit-value flags 14))) (content-encoding (* 8 content-encoding-size) bitstring)
-     (headers-size (* 32 (bit-value flags 13))) (headers (* 8 headers-size) bitstring)
-     (delivery-mode (* 8 (bit-value flags 12)))
-     (priority (* 8 (bit-value flags 11)))
-     (correlation-id-size (* 8 (bit-value flags 10))) (correlation-id (* 8 correlation-id-size) bitstring)
-     (reply-to-size (* 8 (bit-value flags 9))) (reply-to (* 8 reply-to-size) bitstring)
-     (expiration-size (* 8 (bit-value flags 8))) (expiration (* 8 expiration-size) bitstring)
-     (message-id-size (* 8 (bit-value flags 7))) (message-id (* 8 message-id-size) bitstring)
-     (timestamp (* 64 (bit-value flags 6)))
-     (type-size (* 8 (bit-value flags 5))) (type (* 8 type-size) bitstring)
-     (user-id-size (* 8 (bit-value flags 4))) (user-id (* 8 user-id-size) bitstring)
-     (app-id-size (* 8 (bit-value flags 3))) (app-id (* 8 app-id-size) bitstring))
-    (list class-id 0
-          (list (cons 'content-type (bitstring->string content-type))
-                (cons 'content-encoding (bitstring->string content-encoding))
-                (cons 'headers (parse-table headers))
-                (cons 'delivery-mode delivery-mode)
-                (cons 'priority priority)
-                (cons 'correlation-id (bitstring->string correlation-id))
-                (cons 'reply-to (bitstring->string reply-to))
-                (cons 'expiration (bitstring->string expiration))
-                (cons 'message-id (bitstring->string message-id))
-                (cons 'timestamp timestamp)
-                (cons 'type (bitstring->string type))
-                (cons 'user-id (bitstring->string user-id))
-                (cons 'app-id (bitstring->string app-id)))))))
 
 ;; Send an AMQP message over the wire, thread safe
 (define (amqp-send channel type payload)
@@ -245,7 +137,7 @@
                          (cond
                           ((equal? 10 method-id)
                            (amqp-send channel 1
-                                      (amqp:make-connection-start-ok  '(("connection_name" . "my awesome client"))
+                                      (make-connection-start-ok  '(("connection_name" . "my awesome client"))
                                                                       "PLAIN"
                                                                       "\x00local\x00panda4ever"
                                                                       "en_US")))
@@ -253,11 +145,11 @@
                            (let* ((args (frame-properties frm)))
                              (connection-parameters-set! conn args)
                              (amqp-send channel 1
-                                        (amqp:make-connection-tune-ok (alist-ref 'channel-max args)
+                                        (make-connection-tune-ok (alist-ref 'channel-max args)
                                                                       (alist-ref 'frame-max args)
                                                                       (alist-ref 'heartbeat args)))
                              (amqp-send channel 1
-                                        (amqp:make-connection-open "/"))))
+                                        (make-connection-open "/"))))
                           ((equal? 41 method-id)
                            (set! done #t)))
                          (if done
@@ -297,22 +189,22 @@
   (let* [(id (next-channel-id conn))
          (mb (dispatch-register! conn `((channel-id . ,id) (type . 1))))
          (ch (make-channel id mb conn))]
-    (amqp-send ch 1 (amqp:make-channel-open))
+    (amqp-send ch 1 (make-channel-open))
     (amqp-expect ch 20 11)
     ch))
 
 (define (queue-declare channel queue #!key (passive 0) (durable 0) (exclusive 0) (auto-delete 0) (no-wait 0))
-  (amqp-send channel 1 (amqp:make-queue-declare queue passive durable exclusive auto-delete no-wait '()))
+  (amqp-send channel 1 (make-queue-declare queue passive durable exclusive auto-delete no-wait '()))
   (let [(reply (amqp-expect channel 50 11))]
     (alist-ref 'queue (frame-properties reply))))
 
 (define (queue-bind channel queue exchange routing-key . args)
-  (amqp-send channel 1 (amqp:make-queue-bind queue exchange routing-key
+  (amqp-send channel 1 (make-queue-bind queue exchange routing-key
                                              (get-keyword no-wait: args default-0)
                                              '()))
   (amqp-expect channel 50 21))
 
 (define (basic-consume channel queue  #!key (no-local 0) (no-ack 0) (exclusive 0) (no-wait 0))
   (let [(tag "2abc")]
-    (amqp-send channel 1 (amqp:make-basic-consume queue tag no-local no-ack exclusive no-wait '()))
+    (amqp-send channel 1 (make-basic-consume queue tag no-local no-ack exclusive no-wait '()))
     (amqp-expect channel 60 21)))
